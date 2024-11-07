b283bb377c2fc281d950fbff660c9eb93ea8f8c9
Xodeing <codingonly73@gmail.com>
2024-11-07 13:02:33 +0700

    perbaikan main

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -46,7 +46,7 @@ export class AuthController {
       httpOnly: true,
       secure: false, // Ubah menjadi true jika Anda menggunakan HTTPS
     });
-    res.redirect('http://localhost:3000/auth/google/callback');
+    res.redirect('http://localhost:3000');
   }
 
   @ApiBearerAuth()
