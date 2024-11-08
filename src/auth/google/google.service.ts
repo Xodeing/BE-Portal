7946e1ce0eea5d3ecb4cf490f948dbe6b8a9da88
Xodeing <codingonly73@gmail.com>
2024-11-08 22:25:47 +0700

    pembaruan

--- src/auth/google/google.service.ts
+++ src/auth/google/google.service.ts
@@ -19,7 +19,7 @@ export class GoogleService {
           code,
           client_id: process.env.GOOGLE_CLIENT_ID,
           client_secret: process.env.GOOGLE_CLIENT_SECRET,
-          redirect_uri: 'http://localhost:3000',
+          redirect_uri: 'http://localhost:3000/auth/google/callback',
           grant_type: 'authorization_code',
         },
       );
