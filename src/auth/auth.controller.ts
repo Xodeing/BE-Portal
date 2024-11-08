7b491135a3209206dac66568afb6ceeeea1f00cb
Xodeing <codingonly73@gmail.com>
2024-11-08 21:08:39 +0700

    pembaruann

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export class AuthController {
       httpOnly: true,
       secure: false,
     });
-    res.redirect('http://localhost:3000/auth/google/callback');
+    res.redirect('http://localhost:3000');
   }
 
   @ApiTags('Reset Password')
